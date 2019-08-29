a89e9f309205871f5e27a05ba0fe2706e88eb18f
Randy Warrington <warrington.randy@outlook.com>
2019-08-29 13:34:18 -0400

    MVP completed

---
+++ schemes/scheme-model.js
@@ -0,0 +1,45 @@
+const db = require('../data/db')
+
+
+module.exports= {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+  }
+  
+  function find(){
+    return db('schemes');
+  }
+  
+  function findById(id){
+    return db('schemes')
+      .where({ id })
+      .first()
+  }
+  
+  function findSteps(id){
+    return db('schemes as sc')
+      .innerJoin('steps as st', 'st.scheme_id', '=', 'sc.id')
+      .select('st.id', 'st.step_number', 'st.instructions','sc.id')
+      .orderBy('st.step_number')
+      .where({ scheme_id: id})
+  }
+  
+  function add(scheme){
+    return db('schemes').insert(scheme);
+  }
+  
+  function update(changes, id){
+    return db('schemes')
+      .where({ id })
+      .update(changes);
+  }
+  
+  function remove(id){
+    return db('schemes')
+      .where({ id })
+      .del();
+  }
